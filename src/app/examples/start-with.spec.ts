4d0eba939ac24d45e74ac1e375ae7d3a7100aa67
Logan Page <page.lg@gmail.com>
2022-12-15 12:35:34 +0200

    feat: add startWith example

---
+++ src/app/examples/start-with.spec.ts
@@ -0,0 +1,138 @@
+import { cold } from 'jasmine-marbles';
+import { MockService } from 'ng-mocks';
+import { first, map, of, tap } from 'rxjs';
+import { TestBed } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { InputStream } from '../core';
+import { ExecutorService, RuntimeService, StreamBuilderService } from '../services';
+import { StartWithExample } from './start-with';
+
+describe('StartWithExample', () => {
+  let example: StartWithExample;
+  let executorSvc: ExecutorService;
+  let streamBuilderSvc: StreamBuilderService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        StartWithExample,
+        ExecutorService,
+        StreamBuilderService,
+        { provide: MatSnackBar, useValue: MockService(MatSnackBar) },
+        { provide: RuntimeService, useValue: MockService(RuntimeService, { exampleSize$: of<'small' | 'large'>('large') }) },
+      ]
+    });
+
+    example = TestBed.inject(StartWithExample);
+    executorSvc = TestBed.inject(ExecutorService);
+    streamBuilderSvc = TestBed.inject(StreamBuilderService);
+  });
+
+  it('should be created', () => {
+    expect(example).toBeTruthy();
+  });
+
+  describe('desktop', () => {
+    const one = '   --1-2---3------|';
+    const output = 'A-B-C---D------|';
+
+    const values = { A: 'a', B: '1', C: '2', D: '3' };
+
+    let code: string;
+    let inputStreams: InputStream[];
+
+    beforeEach(() => {
+      streamBuilderSvc.setFrames('large');
+
+      code = example.getCode();
+      inputStreams = example.getInputStreams().large;
+    });
+
+    describe('example', () => {
+      it('has expected source stream marbles', () => {
+        expect(inputStreams[0].marbles$).toBeObservable(cold('0', [
+          { marbles: one.trim(), values: null, error: null, canDisplayAsValue: false }
+        ]));
+      });
+
+      it('has expected source stream nodes', () => {
+        const positions = inputStreams.map((stream) => stream.nodes$.pipe(
+          map((nodes) => nodes.map((node) => node.x)),
+        ));
+
+        expect(positions[0]).toBeObservable(cold('0', [[28, 48, 88, 158]]));
+      });
+
+      it('returns expected observable', () => {
+        const result$ = executorSvc.getFunctionResult(code, [cold(one)]);
+        expect(result$).toBeObservable(cold(output, values));
+      });
+    });
+
+    describe('output stream', () => {
+      it('returns the expected observable', (done) => {
+        const result = executorSvc.getVisualizedOutput(of(code), of(inputStreams));
+
+        result.marbles$.pipe(
+          first(),
+          tap(({ marbles, values }) => {
+            expect(marbles).toEqual(output.trim());
+            expect(values).toEqual(values);
+          }),
+        ).subscribe(() => done());
+      });
+    });
+  });
+
+  describe('mobile', () => {
+    const one = '   -1-2--3|';
+    const output = 'AB-C--D|';
+
+    const outputValues = { A: 'a', B: '1', C: '2', D: '3' };
+
+    let code: string;
+    let inputStreams: InputStream[];
+
+    beforeEach(() => {
+      streamBuilderSvc.setFrames('small');
+
+      code = example.getCode();
+      inputStreams = example.getInputStreams().small;
+    });
+
+    describe('example', () => {
+      it('has expected source stream marbles', () => {
+        expect(inputStreams[0].marbles$).toBeObservable(cold('0', [
+          { marbles: one.trim(), values: null, error: null, canDisplayAsValue: false }
+        ]));
+      });
+
+      it('has expected source stream nodes', () => {
+        const positions = inputStreams.map((stream) => stream.nodes$.pipe(
+          map((nodes) => nodes.map((node) => node.x)),
+        ));
+
+        expect(positions[0]).toBeObservable(cold('0', [[18, 38, 68, 78]]));
+      });
+
+      it('returns expected observable', () => {
+        const result$ = executorSvc.getFunctionResult(code, [cold(one)]);
+        expect(result$).toBeObservable(cold(output, outputValues));
+      });
+    });
+
+    describe('output stream', () => {
+      it('returns the expected observable', (done) => {
+        const result = executorSvc.getVisualizedOutput(of(code), of(inputStreams));
+
+        result.marbles$.pipe(
+          first(),
+          tap(({ marbles, values }) => {
+            expect(marbles).toEqual(output.trim());
+            expect(values).toEqual(outputValues);
+          }),
+        ).subscribe(() => done());
+      });
+    });
+  });
+});
