59ef7d5f8f3832cf218ae45a09cc9a429f9a72ec
demensky <dmitry@demensky.com>
2022-12-29 20:51:14 +0200

    feat!: get color scheme from browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,20 @@
-import { tap } from 'rxjs';
-import { OverlayContainer } from '@angular/cdk/overlay';
-import { Component, HostBinding, OnInit } from '@angular/core';
-import { FormControl, NonNullableFormBuilder } from '@angular/forms';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { InsightsService, LocalStorageService, RuntimeService } from './services';
-
-type Theme = 'light' | 'dark';
+import { InsightsService, RuntimeService } from './services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-@UntilDestroy()
-export class AppComponent implements OnInit {
-  protected _defaultTheme: Theme = 'light';
-  protected _themeStorageKey = 'rxTutorTheme';
-
-  darkModeControl: FormControl<boolean> = this._formBuilder.control(false);
+export class AppComponent {
   mediaSize$ = this._runtimeSvc.mediaSize$;
-
-  @HostBinding('class.dark-mode') get darkMode(): boolean {
-    return this.darkModeControl.value;
-  }
 
   constructor(
     protected _router: Router,
-    protected _overlayContainer: OverlayContainer,
-    protected _formBuilder: NonNullableFormBuilder,
-    protected _storageSvc: LocalStorageService,
     protected _insightsSvc: InsightsService,
     protected _runtimeSvc: RuntimeService,
   ) {
     this._insightsSvc.init(this._router);
-  }
-
-  ngOnInit(): void {
-    this.darkModeControl.valueChanges.pipe(
-      tap((darkMode) => {
-        const theme: Theme = darkMode ? 'dark' : 'light';
-        this._storageSvc.saveValue(this._themeStorageKey, theme);
-        if (darkMode) {
-          this._overlayContainer.getContainerElement().classList.add('dark-mode');
-        } else {
-          this._overlayContainer.getContainerElement().classList.remove('dark-mode');
-        }
-      }),
-      untilDestroyed(this),
-    ).subscribe();
-
-    const theme = this._storageSvc.getValue(this._themeStorageKey) as Theme;
-    this.darkModeControl.setValue(theme === 'dark');
   }
 }
